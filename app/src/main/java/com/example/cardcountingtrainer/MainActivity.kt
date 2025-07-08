4cd9625bd4c3f8c583e8d7de4c92e85e7c2cccab
joshdsauter <josh.d.sauter@gmail.com>
2025-07-07 22:16:32 -0400

    Remove default filler in main activity file

--- app/src/main/java/com/example/cardcountingtrainer/MainActivity.kt
+++ app/src/main/java/com/example/cardcountingtrainer/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.cardcountingtrainer.ui.theme.CardCountingTrainerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CardCountingTrainerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CardCountingTrainerApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +27,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CardCountingTrainerTheme {
-        Greeting("Android")
+fun CardCountingTrainerApp(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        // Your content
     }
 }
